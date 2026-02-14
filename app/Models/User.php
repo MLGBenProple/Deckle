45ef6d6e96df2771384b775077ac811215e36085
MLGBenProple <benjaminnpople@hotmail.co.uk>
2026-02-14 00:19:26 +0000

    Remove unused components and tests
    - Deleted InputOTPGroup, InputOTPSeparator, InputOTPSlot, and related files from the input-otp component directory.
    - Removed Input component and its index file from the input component directory.
    - Deleted Label component and its index file from the label component directory.
    - Removed NavigationMenu components and their index file from the navigation-menu directory.
    - Deleted Separator component and its index file from the separator directory.
    - Removed Sheet components and their index files from the sheet directory.
    - Deleted Skeleton component and its index file from the skeleton directory.
    - Removed Spinner component and its index file from the spinner directory.
    - Deleted Tooltip components and their index file from the tooltip directory.
    - Removed DashboardTest, ExampleTest, and Unit ExampleTest files from the tests directory.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,48 +1,95 @@
-<?php
+<?php<?php
 
-namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+
+namespace App\Models;namespace App\Models;
+
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Notifications\Notifiable;use Illuminate\Foundation\Auth\User as Authenticatable;
+
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
-{
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+
+{class User extends Authenticatable
+
+    /** @use HasFactory<\Database\Factories\UserFactory> */{
+
+    use HasFactory, Notifiable;    /** @use HasFactory<\Database\Factories\UserFactory> */
+
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+
+     * The attributes that are mass assignable.    /**
+
+     *     * The attributes that are mass assignable.
+
+     * @var list<string>     *
+
+     */     * @var list<string>
+
+    protected $fillable = [     */
+
+        'name',    protected $fillable = [
+
+        'email',        'name',
+
+        'password',        'email',
+
+    ];        'password',
+
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
+
+     * The attributes that should be hidden for serialization.    /**
+
+     *     * The attributes that should be hidden for serialization.
+
+     * @var list<string>     *
+
+     */     * @var list<string>
+
+    protected $hidden = [     */
+
+        'password',    protected $hidden = [
+
+        'remember_token',        'password',
+
+    ];        'remember_token',
+
     ];
 
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+
+     * Get the attributes that should be cast.    /**
+
+     *     * Get the attributes that should be cast.
+
+     * @return array<string, string>     *
+
+     */     * @return array<string, string>
+
+    protected function casts(): array     */
+
+    {    protected function casts(): array
+
+        return [    {
+
+            'email_verified_at' => 'datetime',        return [
+
+            'password' => 'hashed',            'email_verified_at' => 'datetime',
+
+        ];            'password' => 'hashed',
+
+    }        ];
+
+}    }
+
 }
