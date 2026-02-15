29f937f9cd654a8d3c293a834974cd777843d861
MLGBenProple <benjaminnpople@hotmail.co.uk>
2026-02-15 14:00:54 +0000

    Add Guess button to CommanderTournament page

--- resources/js/pages/CommanderTournament.vue
+++ resources/js/pages/CommanderTournament.vue
@@ -349,6 +349,13 @@ function scryfallImageUrl(cardName: string): string {
                     </button>
                 </div>
             </div>
+            <button
+                class="shrink-0 rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed dark:border-gray-600 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus:ring-offset-gray-800"
+                :disabled="!commanderGuess.trim()"
+                @click="submitGuess()"
+            >
+                Guess
+            </button>
             <span v-if="incorrectGuesses > 0" class="text-sm text-gray-500">
                 {{ incorrectGuesses }} {{ incorrectGuesses === 1 ? 'guess' : 'guesses' }}
             </span>
